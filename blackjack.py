2ef42d226de161ee08e4dfa4c03516877643292b
GustavSandberg <gusa07002@utb.vaxjo.se>
2024-11-13 14:00:49 +0100

    Nearly completet the game

--- blackjack.py
+++ blackjack.py
@@ -20,11 +20,17 @@ def get_color(card):
     else:
         return "♣︎"
 
-def fun():
-    if deal_2_Player == 22:
+def DP_Ace_Ace():
+    if deal_2_Player  == 22:
         return 12
     else:
-        return deal_2_Player
+        return deal_2_Player 
+
+def DD_Ace_Ace():
+    if deal_2_Dealer == 22:
+        return 12
+    else:
+        return deal_2_Dealer 
 
 cards = [x for x in range(1,53)]
 
@@ -44,12 +50,34 @@ else:
 print(f"{deal_1_Dealer} Dealer's Hand")
 
 while True:
+    if deal_2_Player > 21:
+        break
+    if deal_2_Player == 21:
+        break
     Player_input = input("Stand or hit? (s/h): ") 
     if Player_input.lower() == "h":
-        deal_2_player += deal_2_Player + get_value(cards.pop())
-        print(deal_2_player)
+        deal_2_Player =+ deal_2_Player + get_value(cards.pop())
+        print(deal_2_Player)
     else: 
         break
 
-print(deal_2_player)
-print(deal_2_Dealer)
+if deal_2_Player > 21:
+    print("Bust")
+elif deal_2_Player == 21:
+    print("Blackjack")
+else:
+    print(deal_2_Player)
+    print(deal_2_Dealer)
+
+while True:
+
+    if 21 == deal_2_Dealer >= 17: 
+        print("Dealer stands")
+        break
+    elif deal_2_Dealer <= 17:
+        print("Dealer hits")
+        deal_2_Dealer =+ deal_2_Dealer + get_value(cards.pop())
+        print(deal_2_Dealer)
+    else:
+        print("Dealer is bust")
+        break
