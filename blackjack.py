5a058630ec91a0e8626443412d1fa2dd6994f8c4
GustavSandberg <gusa07002@utb.vaxjo.se>
2024-11-15 09:32:01 +0100

    win con

--- blackjack.py
+++ blackjack.py
@@ -32,6 +32,7 @@ def DD_Ace_Ace():
     else:
         return deal_2_Dealer 
 
+
 cards = [x for x in range(1,53)]
 
 random.shuffle(cards)
@@ -39,11 +40,21 @@ random.shuffle(cards)
 deal_1_Player =  get_value(cards.pop()) 
 deal_1_Dealer =  get_value(cards.pop())
 
+if deal_1_Dealer == 11 or 10:
+    print("Dealer can have Blackjack")
+else:
+    print("")
+
 deal_2_Player = deal_1_Player + get_value(cards.pop())
 deal_2_Dealer = deal_1_Dealer + get_value(cards.pop())
 
+
+#if deal_2_Dealer == 21:
+#avsluta splet och dealer vann 
+
+
 if deal_2_Player == 21:
-    print("Blackjack")
+    print("Blackjack")#avsluta splet dubbla bet
 else:
     print(f"{deal_2_Player} Player's Hand") 
 
@@ -71,13 +82,30 @@ else:
 
 while True:
 
-    if 21 == deal_2_Dealer >= 17: 
-        print("Dealer stands")
-        break
-    elif deal_2_Dealer <= 17:
+    if deal_2_Dealer < 17:
         print("Dealer hits")
         deal_2_Dealer =+ deal_2_Dealer + get_value(cards.pop())
         print(deal_2_Dealer)
-    else:
+    elif 21 == deal_2_Dealer >= 17: 
+        print("Dealer stands")
+        break
+    elif deal_2_Dealer > 21:
         print("Dealer is bust")
         break
+    else:
+        break
+
+
+#skapa en granskning om betet 
+if 21 >= deal_2_Player > deal_2_Dealer:
+    print("Player win")#dubbla bet
+elif deal_2_Dealer > 21 and deal_2_Player <= 21:
+    print("Player win")#dubbla bet
+elif 21 >= deal_2_Dealer > deal_2_Player:
+    print("DEALER WIN")#förlora bet
+elif deal_2_Player > 21 and deal_2_Dealer <= 21:
+    print("DEALER WIN")   #förlora bet
+elif deal_2_Dealer == deal_2_Player:
+    print("player is pushed") #få tillbaka bet
+else:
+    print("how") #how
